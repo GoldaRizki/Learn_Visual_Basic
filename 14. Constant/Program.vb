35eb3b3aad100a4ddc1c1bf9a398e035df0f4abf
goldarizki <goldarizki@gmail.com>
2025-01-17 00:21:19 +0700

    update progress constant n array

---
+++ 14. Constant/Program.vb
@@ -0,0 +1,23 @@
+
+
+Module Konstanta
+
+    Sub Main()
+        ' Cons kuwi kanggo nggawe konstanta 
+
+        Dim variabel1 As String = "iki iso diganti"
+
+        Const variabel2 As String = "Iki ga iso diganti, jajalen nek gapercoyo"
+
+        variabel1 = variabel2
+
+        Console.WriteLine(variabel1)
+        Console.WriteLine(variabel2)
+
+
+
+    End Sub
+
+
+
+End Module
